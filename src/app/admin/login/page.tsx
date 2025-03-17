1e0cf3c083202684d5d6696f708a8a1ae538d8eb
SushAN766 <sushanthbs766@gmail.com>
2025-03-17 21:30:51 +0530

    logo changed

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -7,7 +7,7 @@ const Login = () => {
   return (
     <div className="h-[100vh] w-full flex items-center justify-center bg-cover bg-center bg-no-repeat relative"
   style={{
-    backgroundImage: 'url("/home/home-bg.png")'
+    backgroundImage: 'url("/home/home-bg.jpeg")'
   }}
 >
   <div className="absolute inset-0 bg-black bg-opacity-50 backdrop-blur-2xl z-0"></div>
